7cfce25ae7dc6d2e779aaa1278c75091a48b7c02
AndersonOlliveira <anderson.deolliveira@hotmail.com>
2026-05-29 17:28:01 -0300

    criado rota simples para a retorno da conexao com api para testes

--- App/Controllers/Api/ApiHomeController.php
+++ App/Controllers/Api/ApiHomeController.php
@@ -10,12 +10,19 @@ class ApiHomeController
 
     public function testeConection()
     {
+        header("Access-Control-Allow-Origin: http://localhost:5173");
+        header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+        header("Access-Control-Allow-Headers: Content-Type, Authorization");
+        header("Content-Type: application/json; charset=utf-8");
+
 
         $lista = new Mobilidades();
 
         $dados = $lista->lista_mobilidades();
-        echo '<pre>';
-        print_r($dados);
-        echo '</pre>';
+
+        header('Content-Type: application/json');
+
+        echo json_encode($dados);
+        exit;
     }
 }
